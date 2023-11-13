cf680b71513025802b437bc5cfe51d297608a17c
Kuba Sobolewski <101324406+KubaSobolewski@users.noreply.github.com>
2023-11-14 00:46:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from pathlib import Path
 temp = pathlib.PosixPath
 pathlib.PosixPath = pathlib.WindowsPath
 
-filename = "C:\\Users\\Kuba\\Downloads\\model.sv"
+filename = "model.sv"
 model = pickle.load(open(filename,'rb'))
 # otwieramy wcześniej wytrenowany model
 
